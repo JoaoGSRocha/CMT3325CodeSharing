b5ea623fad6a685c78d1a17652c730d88cf89ae4
JoaoGSRocha <joaogsrocha@gmail.com>
2015-02-17 13:52:05 +0000

    N.W.E.S. buttons add & removed enter buttons and Text
    North, West East and South Buttons buttons  added and removal of Enter
    button and Text Input

--- Amazed/Amazed/Amazed/Game.Designer.cs
+++ Amazed/Amazed/Amazed/Game.Designer.cs
@@ -28,27 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
             this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btnMoveSouth = new System.Windows.Forms.Button();
+            this.btnMoveNorth = new System.Windows.Forms.Button();
+            this.btnMoveWest = new System.Windows.Forms.Button();
+            this.btnMoveEast = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(12, 508);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(424, 20);
-            this.textBox1.TabIndex = 0;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(492, 507);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 20);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Enter";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // textBox2
             // 
@@ -63,15 +48,57 @@
             this.textBox2.Size = new System.Drawing.Size(817, 489);
             this.textBox2.TabIndex = 2;
             // 
+            // btnMoveSouth
+            // 
+            this.btnMoveSouth.Location = new System.Drawing.Point(385, 503);
+            this.btnMoveSouth.Name = "btnMoveSouth";
+            this.btnMoveSouth.Size = new System.Drawing.Size(75, 20);
+            this.btnMoveSouth.TabIndex = 3;
+            this.btnMoveSouth.Text = "Move South";
+            this.btnMoveSouth.UseVisualStyleBackColor = true;
+            this.btnMoveSouth.Click += new System.EventHandler(this.btnMoveSouth_Click);
+            // 
+            // btnMoveNorth
+            // 
+            this.btnMoveNorth.Location = new System.Drawing.Point(385, 430);
+            this.btnMoveNorth.Name = "btnMoveNorth";
+            this.btnMoveNorth.Size = new System.Drawing.Size(75, 20);
+            this.btnMoveNorth.TabIndex = 4;
+            this.btnMoveNorth.Text = "Move North";
+            this.btnMoveNorth.UseVisualStyleBackColor = true;
+            this.btnMoveNorth.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // btnMoveWest
+            // 
+            this.btnMoveWest.Location = new System.Drawing.Point(310, 466);
+            this.btnMoveWest.Name = "btnMoveWest";
+            this.btnMoveWest.Size = new System.Drawing.Size(75, 20);
+            this.btnMoveWest.TabIndex = 5;
+            this.btnMoveWest.Text = "Move West";
+            this.btnMoveWest.UseVisualStyleBackColor = true;
+            this.btnMoveWest.Click += new System.EventHandler(this.btnMoveWest_Click);
+            // 
+            // btnMoveEast
+            // 
+            this.btnMoveEast.Location = new System.Drawing.Point(461, 466);
+            this.btnMoveEast.Name = "btnMoveEast";
+            this.btnMoveEast.Size = new System.Drawing.Size(75, 20);
+            this.btnMoveEast.TabIndex = 6;
+            this.btnMoveEast.Text = "Move East";
+            this.btnMoveEast.UseVisualStyleBackColor = true;
+            this.btnMoveEast.Click += new System.EventHandler(this.btnMoveEast_Click);
+            // 
             // Game
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
             this.ClientSize = new System.Drawing.Size(841, 540);
+            this.Controls.Add(this.btnMoveEast);
+            this.Controls.Add(this.btnMoveWest);
+            this.Controls.Add(this.btnMoveNorth);
+            this.Controls.Add(this.btnMoveSouth);
             this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox1);
             this.Name = "Game";
             this.Text = "Amazed";
             this.ResumeLayout(false);
@@ -81,8 +108,10 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btnMoveSouth;
+        private System.Windows.Forms.Button btnMoveNorth;
+        private System.Windows.Forms.Button btnMoveWest;
+        private System.Windows.Forms.Button btnMoveEast;
     }
 }
